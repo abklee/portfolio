38df8eaf6f81a4edc57989072fc60b432c3af749
Alex Klee <alex.klee111@gmail.com>
2026-06-02 14:01:59 -0400

    new posts

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -12,8 +12,7 @@ function Banner() {
         <p className="banner-description">
           I’m an engineer and mathematician based in Virginia, focused on how software works, why it breaks, and how to make it better.
           <br /><br />
-          I'm a recent graduate of Virginia Tech with degrees in computer science and mathematics, and I'm back in Blacksburg for 
-          the 2025-26 school year to wrap up my accelerated master of engineering in computer science.  
+          I'm a recent graduate of Virginia Tech, receiving my masters in Computer Science in May 2026 and bachelors degrees in computer science and mathematics in 2025. Following my graduation I'll be heading up to northern Virginia to return to working at Peraton as a software engineer.     
           <br /><br />
           This site is a collection of things I find worth sharing, research, projects, work, and some occasional fun. Please feel free to contact me about anything you see on here, my email and socials are at the bottom of the page.
         </p>
