05b7ad5b555064a3dac104ff036e954386fbdfec
Alex Klee <alex.klee111@gmail.com>
2026-08-23 20:39:42 -0400

    small edit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,12 +9,10 @@ function Banner() {
       </div>
       <div className="banner-text">
         <h1 className="banner-title">Hi, I'm Alex</h1>
-        <p className="banner-description">
-          I’m an engineer and mathematician based in Virginia, focused on how software works, why it breaks, and how to make it better.
-          <br /><br />
-          I'm a recent graduate of Virginia Tech, receiving my masters in Computer Science in May 2026 and bachelors degrees in computer science and mathematics in 2025. Following my graduation I'll be heading up to northern Virginia to return to working at Peraton as a software engineer.     
-          <br /><br />
-          This site is a collection of things I find worth sharing, research, projects, work, and some occasional fun. Please feel free to contact me about anything you see on here, my email and socials are at the bottom of the page.
+       <p className="banner-description">
+          I’m a software engineer based in Northern Virginia, currently working at Peraton. I'm also a recent graduate of Virginia Tech, receiving my masters in Computer Science in May 2026 and bachelors degrees in computer science and mathematics in 2025. 
+          <br/><br/>
+          This website is a collection of things I find are worth sharing, ranging from cool projects or research I’m working on to photos from fun trips. Please feel free to contact me about anything you see on here, my email and socials are at the bottom of the page.
         </p>
       </div>
     </div>
